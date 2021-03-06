29cb9992ecbf96841e4dab2732c104be5aa8163d
Neeraj Basnet <basnetneeraj@gmail.com>
2021-03-06 22:42:36 +0530

    Update

--- JAVA/src/com/test/selenium/ActionsSelenium.java
+++ JAVA/src/com/test/selenium/ActionsSelenium.java
@@ -6,18 +6,23 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
-public class Wait {
+public class ActionsSelenium {
+	
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "D:\\Automation\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); //Implicit Wait
 		driver.get("https://formy-project.herokuapp.com/");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		Actions action = new Actions(driver);
 		WebElement completeWebFormElement = driver.findElement(By.xpath("//div/li//a[text()='Complete Web Form' and @href='/form']"));
+		action.moveToElement(completeWebFormElement).build().perform();
 		completeWebFormElement.click();
-		WebElement firstNamElement = driver.findElement(By.id("first-name"));
-		firstNamElement.sendKeys("Neeraj");
+		driver.quit();
+
 	}
+	
 
 }
