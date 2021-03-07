5c40642e4ec690683b893d8ee9b217123e5124d3
Neeraj Basnet <basnetneeraj@gmail.com>
2021-03-07 22:51:27 +0530

    Commit

--- JAVA/src/com/test/selenium/AllElements.java
+++ JAVA/src/com/test/selenium/AllElements.java
@@ -3,28 +3,49 @@ package com.test.selenium;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class AllElements {
-	@Test
-	public void prerequisite() {
-		System.setProperty("webdriver.chrome.driver", "D:\\Automation\\chromedriver_win32\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+	
+	 public WebDriver driver;
+	 public String baseULR = "https://formy-project.herokuapp.com/";
+	 public String driverPath = "D:\\Automation\\chromedriver_win32\\chromedriver.exe";
+			 
+	
+	@BeforeTest 
+	
+	public void launchBrowser() {
+		System.setProperty("webdriver.chrome.driver", driverPath);
+		driver = new ChromeDriver();
 		driver.manage().window().maximize();
+	}
+	
+	@Test(priority = 1, description = "Prerequisite action before Test run")
+	
+	//TestNG Annotation, Priority & Description
+	public void completeWebForm() throws InterruptedException {
+		driver.get(baseULR);
+		System.out.println("Browser Launched Successfully");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.get("https://formy-project.herokuapp.com/");
-		driver.findElement(By.xpath("//div/li//a[text()='Complete Web Form']")).click();
+		WebElement completewebformlink = driver.findElement(By.xpath("//div/li//a[text()='Complete Web Form']"));
+		completewebformlink.click();
+		System.out.println("Clicked on Complete Web Form");
+		Thread.sleep(2000);
+		String expectedTitle = "Formy";
+		String actualTitleString = driver.getTitle();
+		Assert.assertEquals(actualTitleString, expectedTitle);
+	}
+		
+	@AfterTest
+	
+	public void terminateBrowser() {
+		driver.quit();
 	}
 	
-	public void getSite() {
-		AllElements obj = new AllElements();
-	}
-	
-	public static void main(String[] args) {
-		
-		
-		
-	}
 
 }
